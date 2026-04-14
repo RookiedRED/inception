eb88e8b437ccf3443a68c11a85bfc4c9efb06920
Oracle Lin <hongyulin715@gmail.com>
2026-04-14 15:42:57 -0500

    Initial Commit

---
+++ inception/inceptionApp.swift
@@ -0,0 +1,17 @@
+//
+//  inceptionApp.swift
+//  inception
+//
+//  Created by Lin, Hung Yu on 4/14/26.
+//
+
+import SwiftUI
+
+@main
+struct inceptionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
